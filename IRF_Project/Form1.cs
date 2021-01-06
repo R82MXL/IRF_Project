112dcd304d3996dfba8c8d54207837f242cdd0bc
R82MXL <balazs.ronto@stud.uni-corvinus.hu>
2021-01-06 22:08:39 +0100

    readCsv

--- IRF_Project/Form1.cs
+++ IRF_Project/Form1.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CsvHelper;
 using IRF_Project.Entities;
 
 namespace IRF_Project
 {
     public partial class Form1 : Form
     {
+        List<Person> personList = new List<Person>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void readCsv()
+        {
+            using (StreamReader csvFile = System.IO.File.OpenText("C:\\Temp\\population.csv"))
+            {
+                CsvReader csv = new CsvReader(csvFile);
+                csv.Configuration.IgnoreHeaderWhiteSpace = true;
+                personList = csv.GetRecords<Person>().ToList();
+                dataGridView.DataSource = personList;
+            }
         }
     }
 }
